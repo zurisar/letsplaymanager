a4b7c67f6ae7df6b0fa581d37503e110c847bbaf
zurisar <zurisar@gmail.com>
2026-09-03 09:45:56 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -12,6 +12,7 @@ pyinstaller --noconsole --onedir --name "LetsPlayManager" main.py
 :: 3. Копируем внешние файлы прямо в корень собранной программы
 echo Копирование внешних ресурсов...
 xcopy /E /I /Y "lang" "dist\LetsPlayManager\lang"
+xcopy /E /I /Y "bin" "dist\LetsPlayManager\bin"
 copy /Y "*.txt" "dist\LetsPlayManager\"
 
 echo.
